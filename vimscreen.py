dd63b89a060c45e37295c1520cb9e28ee9cb0662
garywill <garywill@disroot.org>
2022-12-03 19:45:42 +0800

    gaussian threshold binarize

--- vimscreen.py
+++ vimscreen.py
@@ -15,6 +15,11 @@ def main():
     ss_img = ImageGrab.grab((0, 0, screenW, screenH))
     ss_img.save("/tmp/s.jpg")
 
+    imgOrig = np.asarray(ss_img)
+    imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
+    imgThrW = cv2.adaptiveThreshold(imgGray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY , 3 ,  3)
+    cv2.imwrite('/tmp/2thrw.png' , imgThrW)
+    
  
     
 if __name__ == '__main__':
