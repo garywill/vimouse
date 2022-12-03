1e86257a5cf1b819d3e32a3117c8c589538c946e
garywill <garywill@disroot.org>
2022-12-03 19:54:54 +0800

    use tkinter to show screenshot in borderless window

--- vimscreen.py
+++ vimscreen.py
@@ -2,15 +2,48 @@
 
 import time
 
-from PIL import ImageGrab
 import numpy as np
 import cv2 as cv
 
 cv2 = cv
 
+from PIL import ImageGrab, ImageTk
+import tkinter as tk
 screenW = 1300
 screenH = 600
+wd = None
 
+
+def destroyWindow () :
+    print("destroyWindow()")
+    global wd
+    try:
+        wd.destroy()
+        wd=None
+    except:
+        print("ERROR destroying window")
+    
+def createWindow(w, h) :
+
+    root = tk.Tk()
+    # tlv = tk.Toplevel(root)
+
+    root.overrideredirect(True) # no border
+    root.geometry("%dx%d" % (w,h ) )
+
+
+
+    # closeBtn = tk.Button(root, text = "Close", command = lambda: root.destroy())
+    # closeBtn.place(x=40, y=20)
+
+    root.attributes('-topmost', 1)
+    root.attributes('-alpha', 0.7)
+    root.wait_visibility(root)
+    root.wm_attributes('-alpha',0.7)
+    
+    # root.bind("<Button-3>", destroyWindow)
+        
+    return root
 def invImg(img) :
     return cv2.bitwise_not(img)
 
@@ -37,6 +70,8 @@ def mserImg(img, bgImg) :
 
 
 def main():
+    
+    global wd
     
     
     ss_img = ImageGrab.grab((0, 0, screenW, screenH))
@@ -67,6 +102,17 @@ def main():
     
     print( len (regions) )
     
+    
+    wd = createWindow(screenW, screenH)
+    
+    wdBgImg = ImageTk.PhotoImage(ss_img)
+    
+    canvas = tk.Canvas(wd, width= screenW, height= screenH)
+    canvas.place(x=0, y=0)
+    canvas.create_image(0, 0, image=wdBgImg, anchor=tk.NW)
+    
+    wd.update()
+    # wd.mainloop()
     
 if __name__ == '__main__':
     main()
